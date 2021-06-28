8a538b101644384b6347ef7fe8d98d884ba46165
thiago.gcarneiro <thiago.gcarneiro@stone.com.br>
2021-06-28 11:31:19 -0300

    feature: model

--- src/main/java/com/assessment/recipemanager/model/Recipe.java
+++ src/main/java/com/assessment/recipemanager/model/Recipe.java
@@ -1,13 +1,9 @@
 package com.assessment.recipemanager.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-
 public class Recipe{
 
     @Id
